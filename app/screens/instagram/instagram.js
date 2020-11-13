068c8fbe0e7f536c7f540ba0455c0bb4783a79f8
Julia-Susser <you@example.com>
2020-11-12 20:42:12 -0800

    seventeenth commit

---
+++ app/screens/instagram/instagram.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react'
+import date from 'date-and-time';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Dimensions,
+  Text,
+  Image,
+  Button,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import IconEntypo from 'react-native-vector-icons/Entypo'
+import styles from './instagram-css'
+import {app} from '../../config';
+export default class Instagram extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+     items:[],
+     token: "",
+    };
+  }
+  caption = (item)=>{
+    if (item.caption != undefined){return item.caption}
+  }
+
+  date = (item )=>{
+    var timee = item.timestamp.split("+")[0]
+    var datee = date.transform(timee, 'YYYY-MM-DDTHH:mm:ss', 'MMMM D, YYYY');
+    return (datee)
+  }
+  get_token = () => {
+    const db = app.database();
+    db.ref('instagram').once('value')
+  .then((dataSnapshot) => {
+      this.setState({token: dataSnapshot})
+      console.log(dataSnapshot)
+   });
+   console.log(this.state.token)
+    return (this.state.token)
+  }
+  componentDidMount() {
+
+
+    const db = app.database();
+    db.ref('instagram').once('value')
+  .then((dataSnapshot) => {
+    var link = "https://graph.instagram.com/17841444428002464/media?fields=media_url,caption,timestamp&access_token=https://graph.instagram.com/17841444428002464/media?fields=media_url,caption,timestamp&access_token=IGQVJXcGlzRS15MEtOV1V1ZAlk5eUF5NjVYN0FBZAzI0bGVybllHQ3FSRmpmVkVVeF9nS2ZAIWmoyZAVZAQVjNGMkgzaUFETXVaRlpJbTY0MXh0V0FDWTZAGT0RyZAHRwY3VfY0pReFRqb2Vn"
+    console.log(link)
+    fetch(link).then(response => response.json()).then(data => {
+      this.setState({ items: data["data"]})
+    });
+  })
+  }
+
+  render(){
+
+    return (
+
+<SafeAreaView style={styles.body}>
+  <ScrollView>
+
+    <TouchableOpacity style={{width: 40}} onPress={() => this.props.navigation.navigate('home2')}>
+      <IconEntypo name="chevron-thin-left" size={30}/>
+    </TouchableOpacity>
+
+    <TouchableOpacity onPress={() => this.props.navigation.navigate('home2')}>
+      <View style={styles.imgContainer}>
+        <Image style={styles.logo} source={require('../photos/logo1.png')}/>
+        <Image style={styles.logo2} source={require('../photos/logo2.png')}/>
+      </View>
+    </TouchableOpacity>
+
+    <View>
+      {this.state.items.map((item) =>
+      <View style={styles.box} key={item.id}><Image source={{uri: item.media_url}} style={styles.image}/><Text style={styles.cap}>{this.date(item)}{"\n"}{this.caption(item)}</Text></View>
+      )}
+    </View>
+
+  </ScrollView>
+</SafeAreaView>
+    )
+  }
+}
---
+++ app/screens/signIn/login/login.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import { Text, TextInput, ScrollView, Dimensions,TouchableOpacity, SafeAreaView, Alert, Button, View, StyleSheet } from 'react-native';
+import IconEntypo from 'react-native-vector-icons/Entypo'
+import  AsyncStorage  from '@react-native-community/async-storage'
+import {app} from '../../../config';
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import styles from './login-css'
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      sent: false,
+
+    };
+  }
+  componentDidMount() {
+  }
+  forgotPassword = () =>{
+    var email = this.state.email
+    app.auth().sendPasswordResetEmail(email).then(function() {
+      alert("Sent Reset Password Link to " +email)
+    }).catch(function(error) {
+      alert("Could not send Reset Password Link to " +email + "\n"+error)
+    });
+    this.setState({sent:true})
+  }
+  resetText1 = () =>{
+    if (!this.state.sent){
+    return("Forgot your ")
+  }else{
+    return("Didn't Recieve an email? ")
+  }
+  }
+  resetText2 = () =>{
+    if (!this.state.sent){
+    return("password?")
+  }else{
+    return("Send Again.")
+  }
+}
+  render() {
+    const { navigate } = this.props.navigation;
+    async function saveLoginInfo(name, email, password) {
+      try {
+
+        await AsyncStorage.setItem('email', email)
+        await AsyncStorage.setItem('name', name)
+        await AsyncStorage.setItem('password', password)
+        console.log(name)
+
+      } catch (err){
+        console.log(err)
+      }
+    }
+    return (
+<SafeAreaView style={styles.body}>
+  <ScrollView>
+  <TouchableOpacity style={{width: 40}} onPress={() => navigate('signInOptions')}>
+    <IconEntypo name="chevron-thin-left" size={30}/>
+  </TouchableOpacity>
+
+  <Text style={styles.header}>Log In</Text>
+  <View style={styles.container}>
+    <TextInput
+    value={this.state.email}
+    onChangeText={(email) => this.setState({ email })}
+    placeholder="  Email..."
+    style={styles.input}
+    />
+    <TextInput
+    value={this.state.password}
+    onChangeText={(password) => this.setState({ password })}
+    placeholder="  Password..."
+    secureTextEntry={true}
+    style={styles.input}
+    />
+
+
+    <TouchableOpacity
+    style={styles.sectionContainer2}
+    onPress={() => {
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function(result) {
+    var user = app.auth().currentUser;
+    if (user != null) {
+        var name = user.displayName;
+        var email = user.email;
+    }
+    saveLoginInfo(name, email, password)
+    navigate('home2', {name: name})
+    })
+    .catch(function(error) {
+    console.log("error")
+    alert(error)
+    });}}>
+      <Text style={styles.Text}>Login</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => {this.forgotPassword()}}>
+    <Text style={styles.forgot}> {this.resetText1()}
+
+        <Text style={{textDecorationLine: 'underline',}}>{this.resetText2()}</Text>
+
+    </Text>
+    </TouchableOpacity>
+
+  </View>
+  </ScrollView>
+</SafeAreaView >
+    );
+  }
+}
